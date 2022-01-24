530b75b14357c5f453098ed433d58af04de3ab91
NLCaceres <NLCaceres21@Gmail.com>
2022-01-24 15:04:20 -0800

    Improve code style, add viewbinding, handle display.getSize deprecation

--- app/src/main/java/edu/usc/nlcaceres/infectionprevention/AppFragmentAlertDialog.kt
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/AppFragmentAlertDialog.kt
@@ -1,73 +1,86 @@
 package edu.usc.nlcaceres.infectionprevention
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
-import android.view.Gravity
-import android.view.WindowManager
 import android.graphics.Point
+import android.os.Build
+import android.view.*
+import edu.usc.nlcaceres.infectionprevention.databinding.CustomAlertDialogBinding
+import android.view.WindowInsets
 
-
-
+/* Custom DialogFragment, overrides onCreateView NOT onCreateDialog. Guide: https://bit.ly/3FUyvAy - Dialog with Custom Layout
+Use: Display network errors - Shown by ActivityReportList & ActivityMain */
 class AppFragmentAlertDialog : DialogFragment() {
+  private lateinit var viewBinding : CustomAlertDialogBinding
   private lateinit var alertIcon : ImageView
   private lateinit var alertTitle : TextView
   private lateinit var alertMessage : TextView
   private lateinit var alertOKButton : Button
   private lateinit var alertCancelButton: Button
 
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.custom_alert_dialog, container, false)
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    viewBinding = CustomAlertDialogBinding.inflate(inflater, container, false)
+    return viewBinding.root // https://bit.ly/3mPAtLa - Android Custom Dialog info
+  }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    val args = arguments
-
-    alertIcon = view.findViewById<ImageView>(R.id.alertIcon).apply { setImageDrawable(ContextCompat.getDrawable(context!!, R.drawable.usc_shield_mono_gold)) }
-    alertTitle = view.findViewById<TextView>(R.id.titleTextView).apply { text = args?.getString("Title") }
-    alertMessage = view.findViewById<TextView>(R.id.alertMessage).apply { text = args?.getString("Message") }
-    alertOKButton = view.findViewById<Button>(R.id.alertOkButton).apply { setOnClickListener { dismiss() } }
-    alertCancelButton = view.findViewById<Button>(R.id.alertCancelButton).apply {
-      visibility = if (args?.getBoolean("NeedCancelButton")!!) View.VISIBLE else View.INVISIBLE
+//    alertIcon = view.findViewById<ImageView>(R.id.alertIcon).apply { setImageDrawable(ContextCompat.getDrawable(context, R.drawable.usc_shield_mono_gold)) }
+    alertIcon = viewBinding.alertIcon.apply { setImageDrawable(ContextCompat.getDrawable(context, R.drawable.usc_shield_mono_gold)) }
+    alertTitle = viewBinding.titleTextView.apply { text = arguments?.getString("Title") }
+    alertMessage = viewBinding.alertMessage.apply { text = arguments?.getString("Message") }
+    alertOKButton = viewBinding.alertOkButton.apply { setOnClickListener { dismiss() } }
+    alertCancelButton = viewBinding.alertCancelButton.apply {
+      val needCancelButton = arguments?.getBoolean("NeedCancelButton") ?: false
+      visibility = if (needCancelButton) View.VISIBLE else View.INVISIBLE
       setOnClickListener { dismiss() }
     }
   }
 
   override fun onResume() {
     super.onResume()
-
-    setUpDimensions()
+    dialog?.run { window?.let { setUpDimensions(it) } }
   }
 
-  fun setUpDimensions() {
-    val window = dialog!!.window
-    val size = Point()
+  private fun setUpDimensions(window: Window) {
+    val windowSize = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+      val windowMetrics = window.windowManager.currentWindowMetrics
+      // Gets all insets (system bars, nav bars, display cutouts)
+      val insets = windowMetrics.windowInsets.getInsetsIgnoringVisibility(
+        WindowInsets.Type.navigationBars() or WindowInsets.Type.displayCutout())
+      val insetsWidth: Int = insets.right + insets.left
+      val insetsHeight: Int = insets.top + insets.bottom
 
-    val display = window!!.windowManager.defaultDisplay
-    display.getSize(size)
+      // Subtract away insets so we get ONLY app window size (return Point() rather than Size())
+      Point(windowMetrics.bounds.width() - insetsWidth, windowMetrics.bounds.height() - insetsHeight)
+      // The last expression in a Kotlin if/else blocks get treated like return vals
+    }
+    else { // How display.getSize used to calculate (rather than use currentWindowMetrics.bounds)
+      val display = window.windowManager.defaultDisplay
+      // Why use getSize? Injects the size info into our size var! BUT
+      Point().also { display.getSize(it) } // Deprecated since return val changes if not called in an Activity
+    }
 
-    val width = size.x
-    //val height = size.y
-
+    val width = windowSize.x
+    //val height = windowSize.y
     window.setLayout((width * 0.85).toInt(), WindowManager.LayoutParams.WRAP_CONTENT) // 75% of screen width
-    //window.setLayout((width * 0.85).toInt(), (height * 0.3).toInt()) // Similar version with height modded as well
+    //window.setLayout((width * 0.85).toInt(), (height * 0.3).toInt()) // Similar to above but height modded too
     window.setGravity(Gravity.CENTER)
   }
 
   companion object {
     fun newInstance(title : String, message : String, needCancelButton : Boolean) : AppFragmentAlertDialog {
       val frag = AppFragmentAlertDialog()
-      val args = Bundle()
-      args.putString("Title", title)
-      args.putString("Message", message)
-      args.putBoolean("NeedCancelButton", needCancelButton)
-      frag.arguments = args
+      frag.arguments = Bundle().apply {
+        putString("Title", title)
+        putString("Message", message)
+        putBoolean("NeedCancelButton", needCancelButton)
+      }
       return frag
     }
   }
