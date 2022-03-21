07a05a63e324afd7f13897bde9ad14ee1ae9e5f8
NLCaceres <NLCaceres21@Gmail.com>
2022-03-21 02:08:04 -0700

    Update gradle, convert ActivityMain to Kotlin, make API service with Retrofit + coroutines

---
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/ActivityMain.kt
@@ -0,0 +1,170 @@
+package edu.usc.nlcaceres.infectionprevention
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import edu.usc.nlcaceres.infectionprevention.adapters.PrecautionAdapter
+import edu.usc.nlcaceres.infectionprevention.data.FilterItem
+import edu.usc.nlcaceres.infectionprevention.data.Precaution
+import edu.usc.nlcaceres.infectionprevention.data.ReportService
+import edu.usc.nlcaceres.infectionprevention.databinding.ActivityMainBinding
+import edu.usc.nlcaceres.infectionprevention.util.createReportPracticeExtra
+import edu.usc.nlcaceres.infectionprevention.util.preSelectedFilterExtra
+import edu.usc.nlcaceres.infectionprevention.util.SetupToolbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.Job
+import retrofit2.HttpException
+
+/* Homepage that allows users to choose a type of health violation that occurred
+ and create a report for it
+ Links to: ActivityCreateReport + ActivityReportList */
+//TODO: Fix recreation when going from CreateReport or Settings back to Main. ReportsList and even Filters work fine
+class ActivityMain : AppCompatActivity() {
+
+  private lateinit var viewBinding : ActivityMainBinding
+  private lateinit var mDrawerLayout: DrawerLayout
+  private lateinit var mProgressIndicator : ProgressBar
+  private lateinit var sorryMsgTextView : TextView
+  private lateinit var precautionRecyclerView : RecyclerView
+  private lateinit var precautionAdapter : PrecautionAdapter
+  private var precautionFetchJob : Job? = null
+  private var precautionList = arrayListOf<Precaution>()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    viewBinding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(viewBinding.root)
+
+    setUpToolbarAndNavView()
+    sorryMsgTextView = viewBinding.sorryTextView
+
+    precautionRecyclerView = viewBinding.precautionRV.apply {
+      setHasFixedSize(true)
+      precautionAdapter = PrecautionAdapter { _, healthPractice ->
+        Intent(applicationContext, ActivityCreateReport::class.java).apply {
+          putExtra(createReportPracticeExtra, healthPractice.name)
+        }.also { createReportActivityLauncher.launch(it) }
+      }
+      adapter = precautionAdapter
+      precautionAdapter.submitList(precautionList)
+    }
+    if (precautionList.size == 0) fetchPrecautions()
+  }
+
+  private fun setUpToolbarAndNavView() {
+    SetupToolbar(this, viewBinding.toolbarLayout.homeToolbar, R.drawable.ic_menu)
+
+    mDrawerLayout = viewBinding.myNavDrawer
+
+    viewBinding.navView.apply {
+      setNavigationItemSelectedListener(NavDrawerHandler())
+      getHeaderView(0).findViewById<Button>(R.id.navCloseButton).setOnClickListener { mDrawerLayout.closeDrawers() }
+    }
+
+    mProgressIndicator = viewBinding.progressIndicatorLayout.appProgressbar.apply { visibility = View.VISIBLE }
+  }
+
+  /* Next 2 funs = Menu Setup */
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    menuInflater.inflate(R.menu.action_buttons, menu)
+    return true // super.onCreateOptionsMenu(menu) // If returns false, menu won't display
+  }
+  override fun onOptionsItemSelected(item: MenuItem): Boolean { // Return true to end any further menu processing
+    return when (item.itemId) {
+      android.R.id.home -> { mDrawerLayout.openDrawer(GravityCompat.START); true }
+      R.id.action_settings -> {
+        Snackbar.make(viewBinding.myCoordinatorLayout, R.string.my_message, Snackbar.LENGTH_SHORT).show()
+        Intent(this, ActivitySettings::class.java).also { startActivity(it) }
+        true
+      }
+      else -> return super.onOptionsItemSelected(item)
+    }
+  }
+
+  private val createReportActivityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+    if (result.resultCode == Activity.RESULT_OK) {
+      startActivity(Intent(applicationContext, ActivityReportList::class.java))
+    }
+  }
+
+  private fun fetchPrecautions() {
+    precautionFetchJob = CoroutineScope(Dispatchers.IO).launch {
+      val precautionResponse = ReportService.createPrecautionApi().fetchPrecautionList()
+      withContext(Dispatchers.Main) {
+        try {
+          if (precautionResponse.isSuccessful) {
+            // Using submitList callback to add new list of items once committed to activity's list
+            precautionResponse.body()?.let { precautionAdapter.submitList(ArrayList(it)) { precautionList.addAll(it) } }
+          }
+          else {
+            Snackbar.make(viewBinding.myCoordinatorLayout, "Error: ${precautionResponse.code()}", Snackbar.LENGTH_SHORT).show()
+            AppFragmentAlertDialog.newInstance(resources.getString(R.string.main_alert_dialog_title), resources.getString(R.string.main_alert_dialog_message),
+              false).show(supportFragmentManager, "main_alert_dialog")
+            sorryMsgTextView.visibility = View.VISIBLE
+          }
+          mProgressIndicator.visibility = View.INVISIBLE // Made invisible no matter what
+        } catch (e : HttpException) {
+          Snackbar.make(viewBinding.myCoordinatorLayout, "HTTP Error: ${e.message()}", Snackbar.LENGTH_SHORT).show()
+        } catch (e : Throwable) {
+          Snackbar.make(viewBinding.myCoordinatorLayout, "Error: ${e.message}", Snackbar.LENGTH_SHORT).show()
+        }
+      }
+    }
+  }
+
+  private inner class NavDrawerHandler : NavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+      val reportListIntent = Intent(applicationContext, ActivityReportList::class.java)
+      when (item.itemId) {
+        R.id.nav_reports -> {
+          Log.d("Nav Item Selected", "Clicked General Report Item")
+        }
+        R.id.nav_standard_precautions -> {
+          reportListIntent.putExtra(preSelectedFilterExtra, FilterItem("Standard", true, "Precaution Type"))
+          Log.d("Nav Item Selected", "Clicked Standard Precautions Report Item")
+        }
+        R.id.nav_isolation_precautions -> {
+          reportListIntent.putExtra(preSelectedFilterExtra, FilterItem("Isolation", true, "Precaution Type"))
+          Log.d("Nav Item Selected", "Clicked Isolation Precautions Report Item")
+        }
+        else -> return false
+      }
+      mDrawerLayout.closeDrawers()
+
+      val precautionNames = arrayListOf<String>()
+      val healthPracticeNames = arrayListOf<String>()
+      for (currentPrecaution in precautionList) {
+        precautionNames.add(currentPrecaution.name)
+        currentPrecaution.practices?.let { healthPracticeList ->
+          for (currentHealthPractice in healthPracticeList) healthPracticeNames.add(currentHealthPractice.name)
+        }
+      }
+      reportListIntent.putStringArrayListExtra("PrecautionList", precautionNames)
+      reportListIntent.putStringArrayListExtra("PracticeList", healthPracticeNames)
+      startActivity(reportListIntent)
+      return true
+    }
+  }
+
+  override fun onStop() {
+    super.onStop()
+    precautionFetchJob?.cancel() // Kill pending coroutines
+  }
+}
---
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/data/Services.kt
@@ -0,0 +1,42 @@
+package edu.usc.nlcaceres.infectionprevention.data
+
+import edu.usc.nlcaceres.infectionprevention.util.baseURL
+import edu.usc.nlcaceres.infectionprevention.util.snakeCaseGson
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.Response
+
+interface ReportService {
+
+  companion object {
+    private fun createBaseRetrofitInstance() : Retrofit {
+      return Retrofit.Builder().baseUrl(baseURL) // BaseUrl must end in '/'
+        .addConverterFactory(GsonConverterFactory.create(snakeCaseGson()))
+        .build()
+    }
+    fun createReportApi() : ReportApiInterface {
+      return Retrofit.Builder().baseUrl(baseURL)
+        .addConverterFactory(GsonConverterFactory.create(snakeCaseGson())) // Custom Gson factory based on a GsonBuilder instance
+        .build().create(ReportApiInterface::class.java)
+    }
+    fun createPrecautionApi() : PrecautionApiInterface {
+      return Retrofit.Builder().baseUrl(baseURL)
+        .addConverterFactory(GsonConverterFactory.create(snakeCaseGson()))
+        .build().create(PrecautionApiInterface::class.java)
+    }
+  }
+
+  interface ReportApiInterface {
+    @GET("reports") // Suspend funs launch on main thread by default so need to call from a coroutine!
+    suspend fun fetchReportList(): Response<List<Report>>
+    @POST("reports/create") // Response helper type gets metadata like status codes
+    suspend fun createReport(@Body report : Report): Response<Report>
+  }
+  interface PrecautionApiInterface {
+    @GET("precautions")
+    suspend fun fetchPrecautionList(): Response<List<Precaution>>
+  }
+}
