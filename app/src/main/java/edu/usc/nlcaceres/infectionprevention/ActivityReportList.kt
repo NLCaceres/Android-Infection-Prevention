bc4a86e543419652eee7eb422d00808efc92f0cd
NLCaceres <NLCaceres21@Gmail.com>
2022-08-04 02:12:27 -0700

    Redo searchbar as editText action view. Implement sort+filter. Cleanup conditionals

--- app/src/main/java/edu/usc/nlcaceres/infectionprevention/ActivityReportList.kt
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/ActivityReportList.kt
@@ -2,27 +2,25 @@ package edu.usc.nlcaceres.infectionprevention
 
 import android.app.Activity
 import edu.usc.nlcaceres.infectionprevention.util.EspressoIdlingResource
-import android.app.SearchManager
-import android.content.Context
 import android.content.Intent
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.InputType.TYPE_CLASS_TEXT
 import android.util.Log
 import android.view.View
 import android.view.Menu
 import android.view.MenuItem
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
-import android.widget.AutoCompleteTextView
-import android.widget.ImageView
+import android.widget.EditText
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import edu.usc.nlcaceres.infectionprevention.adapters.ReportAdapter
@@ -39,7 +37,6 @@ import edu.usc.nlcaceres.infectionprevention.data.Report
 import edu.usc.nlcaceres.infectionprevention.data.FilterItem
 import edu.usc.nlcaceres.infectionprevention.adapters.SelectedFilterAdapter
 import kotlin.collections.ArrayList
-//import java.edu.usc.nlcaceres.infectionprevention.util.Date
 import edu.usc.nlcaceres.infectionprevention.databinding.ActivityReportListBinding
 // Following = FetchReports dependencies TODO Move to a Service class
 import com.google.gson.GsonBuilder
@@ -96,9 +93,9 @@ class ActivityReportList : AppCompatActivity() {
     selectedFilterAdapter = SelectedFilterAdapter { _, _, position -> // View, FilterItem, Int
       selectedFilters.removeAt(position) // First remove filter from selectedFilterList
       selectedFilterAdapter.notifyItemRemoved(position) // Bit more efficient than submitting whole new list to diff
+      beginSortAndFiltering()
     }
     selectedFilterRV = viewBinding.selectedFilterRV.apply {
-      // If our custom Adapter doesn't receive a brand new list then its updateData callback can act odd
       adapter = selectedFilterAdapter // May be worth using apply with submitList in it!
       (adapter as SelectedFilterAdapter).submitList(selectedFilters)
       layoutManager = FlexboxLayoutManager(context, FlexDirection.ROW, FlexWrap.WRAP).apply { justifyContent = JustifyContent.CENTER }
@@ -107,27 +104,26 @@ class ActivityReportList : AppCompatActivity() {
     sorryMessage = viewBinding.sorryTextView // Fallback textview
 
     reportsRV = viewBinding.reportRV.apply {
-      //TODO May need to pass a callback! (updateData() fun in the file! maybe?)
-      reportsAdapter = ReportAdapter().also { adapter = it } // ALSO set adapter of reportsRV
+      reportsAdapter = ReportAdapter().also { adapter = it } // ALSO setAdapter for reportsRV
       addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
     }
 
-    if (reportList.size == 0) { refreshLayout.isRefreshing = true; fetchReports() }
+    if (reportList.isEmpty()) { refreshLayout.isRefreshing = true; fetchReports() }
 
     filterFloatButton = viewBinding.sortFilterFloatingButton.apply { setOnClickListener {
       Intent(context, ActivitySortFilter::class.java).let {
         it.putStringArrayListExtra("PrecautionList", intent?.getStringArrayListExtra("PrecautionList"))
         it.putStringArrayListExtra("PracticeList", intent?.getStringArrayListExtra("PracticeList"))
-        sortFilterActivityLauncher.launch(it) // How to handle Activity changes as of SDK 30 (no more request codes)
+        sortFilterActivityLauncher.launch(it) // How to launch new Activity as of SDK 30 (no more request codes)
       }
     }}
   }
 
-  override fun onBackPressed() { // Guaranteed to fire and immediately! - normally would put the following in onStop
+  override fun onBackPressed() { // Guaranteed to fire (since up+back are only way out) and immediately! Normally would be onStop
     // Application Context better below! No leaky activities!
     RequestQueueSingleton.getInstance(applicationContext).requestQueue.cancelAll(ReportListCancelTag)
     refreshLayout.isRefreshing = true
-    super.onBackPressed() // Calling the super ensures activity finish occurs
+    super.onBackPressed() // Calling the super ensures activity calls finish()
   }
 
   // Set up searchButton + Settings button
@@ -143,69 +139,71 @@ class ActivityReportList : AppCompatActivity() {
   }
 
   private fun setUpSearchView(menu : Menu) {
-    val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
-
-    val searchMenu = menu.findItem(R.id.search_action).apply {
-      icon.colorFilter = PorterDuffColorFilter(ContextCompat.getColor(applicationContext, R.color.colorLight), PorterDuff.Mode.SRC_IN)
+    val searchMenu = menu.findItem(R.id.search_action)
+    val searchBar = (searchMenu?.actionView as EditText).apply {
+      setHint(R.string.search_hint)
+      setHintTextColor(ContextCompat.getColor(this@ActivityReportList, R.color.colorLight))
+      maxLines = 1; inputType = TYPE_CLASS_TEXT // Need BOTH of these to restrict text to single line
+      background.colorFilter = PorterDuffColorFilter(ContextCompat
+        .getColor(this@ActivityReportList, R.color.colorLight), PorterDuff.Mode.SRC_IN)
     }
-
-    // searchInfo sets config in /res/xml module
-    val searchView = (searchMenu?.actionView as SearchView).apply { setSearchableInfo(searchManager.getSearchableInfo(componentName)) }
-
-    // Next 3 blocks set up interactivity of searchView components (autocomplete box + close button + editText)
-    val searchBox = searchView.findViewById<AutoCompleteTextView>(androidx.appcompat.R.id.search_src_text).apply {
-      setHintTextColor(ContextCompat.getColor(applicationContext, R.color.appBackground))
-      setOnFocusChangeListener { view, focused ->
-        if (!focused) (getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(view.windowToken, 0)
-        if (text.isEmpty()) toolbar.collapseActionView() // Close searchView entirely only if empty searchViewTextbox
+    val focusListener = View.OnFocusChangeListener { view, isFocused ->
+      if (!isFocused) { // Whenever searchActionView loses focus close keyboard + close actionView if no query
+        (getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager)
+          .hideSoftInputFromWindow(view.windowToken, 0)
+        (view as? EditText)?.run { if (text.isEmpty()) toolbar.collapseActionView() }
       }
     }
-    searchView.findViewById<ImageView>(androidx.appcompat.R.id.search_close_btn).apply {
-      setOnClickListener { // Mostly what normally happens (with a bonus full closure)
-        searchBox.setText(""); searchView.setQuery("", false) // Query is emptied and not sent out
-        searchView.onActionViewCollapsed(); searchMenu.collapseActionView() // Close actionView then reset widget to icon form
-      } // If needed tie in with searchMenu.setOnActionExpandListener (which has funcs for expansion and closure)
-    }
-    searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-      override fun onQueryTextChange(newText: String): Boolean { // NewText's default = "" (never null)
-        // Alternative is using kotlin coroutines, using a job launched off Dispatchers.Main with a delay and func passed in (so it's also cancellable)
-        handler.removeCallbacks(runnable) // Idea is to kill previous runnables on next text change
-        runnable = Runnable { filterReportsBySearch(newText) } // Order of remove and then setting runnable matters here!
-        handler.postDelayed(runnable, 750) // There4 this will only run if half a second gap in text change
-        return false // False == default suggestions by searchView instead of true == by listener
+    val expansionListener = object : MenuItem.OnActionExpandListener {
+      override fun onMenuItemActionExpand(searchIcon: MenuItem?): Boolean { // Returning true lets expansion/collapse to happen
+        searchBar.requestFocus()
+        if (searchBar.onFocusChangeListener == null) searchBar.onFocusChangeListener = focusListener
+        return true
       }
-      override fun onQueryTextSubmit(query: String?): Boolean = false // False == Handle by SearchView instead of by listener
-    })
+      override fun onMenuItemActionCollapse(searchIcon: MenuItem?): Boolean {
+        if (currentFocus == searchBar && searchBar.text.isNotEmpty()) {
+          searchBar.text.clear()
+          (getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager)
+            .hideSoftInputFromWindow(searchBar.windowToken, 0)
+          searchBar.onFocusChangeListener = null // Prevents double collapse nullException call (listening restarts on next expansion)
+          return true
+        }
+        else if (currentFocus == searchBar) {
+          searchBar.clearFocus(); return false // Clear focus and let focusListener close actionView
+        }
+        // Made it here, either user didn't interact at all or above "else if" ran so clearFocus() let focusListener run collapse
+        (getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager)
+          .hideSoftInputFromWindow(searchIcon?.actionView?.windowToken, 0)
+        searchBar.text.clear(); return true
+      }
+    }
+    searchMenu.setOnActionExpandListener(expansionListener)
+
+    searchBar.doOnTextChanged { newText, _, _, _ -> // newText defaults to "" (never null)
+      EspressoIdlingResource.increment() // Slows instrumentedTests due to delay below but now tests work as expected!
+      // Alternative is using kotlin coroutines, using a job launched off Dispatchers.Main with a delay and func passed in (so it's also cancellable)
+      handler.removeCallbacks(runnable) // Idea is to kill previous runnables on next text change
+      runnable = Runnable { filterReportsBySearch(newText.toString()) } // Order of remove and then setting runnable matters here!
+      handler.postDelayed(runnable, 750) // There4 this will only run if half a second gap in text change
+    }
   }
 
-  // If activity = singleTopLaunchMode then need this for searchView - activity not recreated so no onCreate to grab previous searchIntent
-  override fun onNewIntent(intent: Intent) {
-    super.onNewIntent(intent) // Ensures all the right things fire under the hood
-    if (Intent.ACTION_SEARCH == intent.action) { // Prevents back button from sort and filter list from overwriting intents
-      // setIntent(intent) // If not called, then old intent is kept (which would be useful if needed later)
-      intent.getStringExtra(SearchManager.QUERY)?.also { query ->
-        handler.removeCallbacks(runnable)
-        runnable = Runnable { filterReportsBySearch(query) }
-        handler.postDelayed(runnable, 750)
-      }
-    }
-  }
-
-  private fun filterReportsBySearch(searchText: String) { // Alternatively place in adapter! with copy of array init'd by addAll.
+  private fun filterReportsBySearch(searchText: String) {
     if (searchText.isEmpty()) {
-      // Instead of an updateData() in the adapter, the new ListAdapter has submitList() which autodiffs on a background thread if the RV isn't empty!
+      // Instead of an updateData() in the adapter, the new ListAdapter has submitList() which autodiffs on a background thread if RV not empty!
       reportsAdapter.submitList(reportList)
       if (reportsAdapter.itemCount > 0) reportsRV.scrollToPosition(0) // Checking after update
     }
     else {
       val sortedList = reportList.filter { report -> // If case sensitive, then `str2 in str1` beats str1.contain(str2)
-        report.employee?.fullName?.contains(searchText, true)!! ||
-            report.date.toString().contains(searchText, true) ||
-            report.location.toString().contains(searchText, true) ||
-            report.healthPractice?.name?.contains(searchText, true)!!
-      } as ArrayList<Report>
+        (report.employee?.fullName?.contains(searchText, true) ?: false) ||
+          report.date.toString().contains(searchText, true) ||
+          report.location.toString().contains(searchText, true) ||
+          (report.healthPractice?.name?.contains(searchText, true) ?: false)
+      }
       reportsAdapter.submitList(sortedList)
     }
+    EspressoIdlingResource.decrement()
   }
 
   // TODO: Allow multiple formats to be compared
@@ -218,10 +216,12 @@ class ActivityReportList : AppCompatActivity() {
       try {
         newReportsList.addAll(GsonBuilder().registerTypeAdapter(Report::class.java, ReportDeserializer()).create().
           fromJson(it, TypeToken.getParameterized(ArrayList::class.java, Report::class.java).type))
-      } catch (err : Error) { Log.w("Report Parse Err", "Issue with parsing the json for reports")}
+      } catch (err : Error) { Log.w("Report Parse Err", "Issue with parsing the json for reports") }
 
-      if (reportList.size > 0 ) reportList.clear(); reportList.addAll(newReportsList) // Activity's copy of what adapter gets
+      if (reportList.isNotEmpty()) reportList.clear() // Clear any previous calls' returns
+      reportList.addAll(newReportsList) // Activity also needs a copy
       reportsAdapter.submitList(newReportsList) // notifyDataSetChanged no longer needed (and should never have been)
+      beginSortAndFiltering()
       refreshLayout.isRefreshing = false
       EspressoIdlingResource.decrement()
 
@@ -246,11 +246,47 @@ class ActivityReportList : AppCompatActivity() {
     // Handles result from the Sorting/Filtering Activity
     if (it.resultCode == Activity.RESULT_OK) {
       val selectedFiltersReceived = it.data?.getParcelableArrayListExtra<FilterItem>(selectedFilterParcel)
-      if (selectedFiltersReceived != null && selectedFiltersReceived.size > 0) {
+      if (selectedFiltersReceived != null && selectedFiltersReceived.isNotEmpty()) {
         Snackbar.make(viewBinding.myCoordinatorLayout, "Filtering and Sorting!", Snackbar.LENGTH_SHORT).show()
+
         selectedFilters.clear(); selectedFilters.addAll(selectedFiltersReceived)
         selectedFilterAdapter.notifyItemRangeInserted(0, selectedFiltersReceived.size)
+
+        beginSortAndFiltering()
       }
     }
+  }
+
+  private fun beginSortAndFiltering(reportsList: List<Report> = reportList, chosenFilters: List<FilterItem> = selectedFilters) {
+    val onlySortNeeded = chosenFilters.size == 1 && chosenFilters[0].filterGroupName == "Sort By" // Helps avoid filtering but returning true for everything
+    var sortByIndex = if (onlySortNeeded) 0 else -1
+    val filteredList = if (chosenFilters.isEmpty() || onlySortNeeded) reportsList
+      else reportsList.filter { report ->
+        var keepReport = false
+        chosenFilters.forEachIndexed { i, filter ->
+          if (filter.filterGroupName == "Sort By") sortByIndex = i
+          else {
+            keepReport = filterReportsBy(report, filter.filterGroupName, filter.name)
+            if (keepReport) return@filter keepReport // Early break if we already have a match
+          }
+        }
+        return@filter keepReport
+      }
+    // If we found a sort instruction then call sort so it can return a sorted list to submit! Else submit the filteredList
+    if (sortByIndex in chosenFilters.indices) reportsAdapter.submitList(sortReportsBy(filteredList, chosenFilters[sortByIndex].name))
+    else reportsAdapter.submitList(filteredList)
+  }
+
+  private fun filterReportsBy(report: Report, filterCategory: String, filter: String) = when (filterCategory) {
+    "Precaution Type" -> report.healthPractice?.precaution?.name?.startsWith(filter)
+    "Health Practice Type" -> report.healthPractice?.name?.startsWith(filter)
+    else -> false // Unknown filter, no match. Allow empty results (if results empty, use sorryTextView message?)
+  } ?: false // If a null chain fails, returning null, then Elvis fallback to non-null false
+
+  private fun sortReportsBy(filteredList: List<Report>, sorter: String) = when (sorter) {
+    "Date Reported" -> filteredList.sortedBy { it.date }
+    "Employee Name (A-Z)" -> filteredList.sortedBy { it.employee?.fullName }
+    "Employee Name (Z-A)" -> filteredList.sortedByDescending { it.employee?.fullName }
+    else -> filteredList
   }
 }
