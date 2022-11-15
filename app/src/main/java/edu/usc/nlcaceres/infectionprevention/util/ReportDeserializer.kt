007744ba6ee5538b1204246aed8edcff85390f69
NLCaceres <NLCaceres21@Gmail.com>
2022-11-14 17:17:54 -0800

    Cleanup the Gson ReportDeserializer implementation's usage across app + test

---
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/util/ReportDeserializer.kt
@@ -0,0 +1,32 @@
+package edu.usc.nlcaceres.infectionprevention.util
+
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonDeserializationContext
+import java.lang.reflect.Type
+import edu.usc.nlcaceres.infectionprevention.data.Location
+import edu.usc.nlcaceres.infectionprevention.data.Employee
+import edu.usc.nlcaceres.infectionprevention.data.Report
+import edu.usc.nlcaceres.infectionprevention.data.HealthPractice
+import java.time.Instant
+
+class ReportDeserializer : JsonDeserializer<Report> {
+  override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): Report? {
+    json?.asJsonObject?.run { // Use run instead of let to access contents!
+      val id = get("_id").asString
+      val employee = get("employee").asJsonObject.run {
+        Employee(get("_id").asString, get("first_name").asString, get("surname").asString, null)
+      }
+      val healthPractice = get("healthPractice").asJsonObject.run {
+        HealthPractice(get("_id").asString, get("name").asString, null)
+      }
+      val location = get("location").asJsonObject.run {
+        Location(get("_id").asString, get("facilityName").asString, get("unitNum").asString, get("roomNum").asString)
+      }
+      // Example "2019-05-19T06:36:05.018Z" vs the formatted_date_reported kv-pair
+      val date = Instant.parse(get("date_reported").asString)
+      return Report(id, employee, healthPractice, location, date)
+    }
+    return null
+  }
+}
---
+++ app/src/test/java/edu/usc/nlcaceres/infectionprevention/util/ReportDeserializerTest.kt
@@ -0,0 +1,80 @@
+package edu.usc.nlcaceres.infectionprevention.util
+
+import org.junit.Test
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import edu.usc.nlcaceres.infectionprevention.data.Report
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+
+class ReportDeserializerTest {
+  @Test fun `Check Single Report Deserialization`() { // Grab files from a resources directory in Unit Tests
+    // Option 1 for reading json - getResourceAsStream.bufferedReader() to pass the Gson instance a reader
+    val inputStream = javaClass.classLoader?.getResourceAsStream("report.json")!!
+    // Using .bufferedReader() is easier than passing in a BufferedReader(InputStreamReader(inputStream))
+    // BUT there's a problem! Turns out using a reader is really only useful if you need it ONCE!
+    val inputMarkSupported = inputStream.markSupported() // UNLESS you check if markSupported()
+    // IF supported, mark() the start & pass in available() to get the total # of bytes left to read
+    if (inputMarkSupported) { inputStream.mark(inputStream.available()) } // SO the total byte count!
+    // Passing the full count prevents removing the mark so you can ALWAYS reset back to the start for another read
+    val reader = inputStream.bufferedReader()
+    val reportJson = Gson().fromJson(reader, JsonObject::class.java)
+
+    if (inputMarkSupported) { inputStream.reset() } // Now we can call reset to go to the stream's start where we marked
+
+    val reportGsonConverter = GsonBuilder().registerTypeAdapter(Report::class.java, ReportDeserializer()).create()
+    val report = reportGsonConverter.fromJson(reader, Report::class.java) // Make a Gson instance to use our deserializer
+
+    assertThat(report, IsInstanceOf(Report::class.java))
+    inspectReportJson(reportJson, report, "1")
+  }
+
+  @Test fun `Check Report List Deserialization`() { // Instead of a bufferedReader, there's an easier solution!
+    // Option 2 for reading json - getResource.readText() to pass the Gson instance a string
+    val listInput = javaClass.classLoader?.getResource("reports.json")!!
+    val listText = listInput.readText()
+
+    val reportsArrJson = Gson().fromJson(listText, JsonArray::class.java)
+    val reportsArr = GsonBuilder().registerTypeAdapter(Report::class.java, ReportDeserializer())
+      .create().fromJson<List<Report>>(listText, TypeToken.getParameterized(ArrayList::class.java, Report::class.java).type)
+
+    assertThat(reportsArr, IsInstanceOf(ArrayList::class.java))
+    for (i in reportsArr.indices) {
+      inspectReportJson(reportsArrJson[i] as JsonObject, reportsArr[i], "${i + 1}")
+    }
+  }
+
+  private fun inspectReportJson(reportJson: JsonObject, report: Report, expectedID: String) {
+    assertEquals(reportJson.get("_id").asString, report.id) // Could do assertThat(string, StringContains(someVal)) for flexibility
+    assertEquals(expectedID, report.id)
+
+    assertNotNull(report.employee)
+    val employeeJson = reportJson.get("employee").asJsonObject
+    assertEquals(employeeJson.get("_id").asString, report.employee?.id)
+    assertEquals(expectedID, report.employee?.id)
+    assertEquals(employeeJson.get("first_name").asString, report.employee?.firstName)
+    assertEquals(employeeJson.get("surname").asString, report.employee?.surname)
+
+    assertNotNull(report.healthPractice)
+    val healthPracticeJson = reportJson.get("healthPractice").asJsonObject
+    assertEquals(healthPracticeJson.get("_id").asString, report.healthPractice?.id)
+    assertEquals(expectedID, report.healthPractice?.id)
+    assertEquals(healthPracticeJson.get("name").asString, report.healthPractice?.name)
+
+    assertNotNull(report.location)
+    val locationJson = reportJson.get("location").asJsonObject
+    assertEquals(locationJson.get("_id").asString, report.location?.id)
+    assertEquals(expectedID, report.location?.id)
+    assertEquals(locationJson.get("facilityName").asString, report.location?.facilityName)
+    assertEquals(locationJson.get("unitNum").asString, report.location?.unitNum)
+    assertEquals(locationJson.get("roomNum").asString, report.location?.roomNum)
+
+    val dateJson = reportJson.get("date_reported").asString
+    assertEquals(dateJson, report.date.toString())
+  }
+}
