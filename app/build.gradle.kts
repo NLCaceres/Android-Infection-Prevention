5e285703a6b2476c10dd50f8db32c51777dd3abb
NLCaceres <NLCaceres21@Gmail.com>
2023-12-20 01:28:51 -0800

    Fix odd Kapt issue affecting `Dagger/Hilt FastInit` during build by placing Kapt last in plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,10 +2,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.google.dagger.hilt)
     alias(libs.plugins.androidx.navigation.safeargs.kotlin)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.google.dagger.hilt) apply false
     alias(libs.plugins.androidx.navigation.safeargs.kotlin) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
 }
 
 tasks.register("clean", Delete::class) {
