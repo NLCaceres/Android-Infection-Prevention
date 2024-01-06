d7053097af58d5f922fc46a19f2da1a843bc0888
NLCaceres <NLCaceres21@Gmail.com>
2024-01-06 01:37:35 -0800

    Create SorterFilterListView Screen-level Composable for use in FragmentReportList

---
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/views/SorterFilterListView.kt
@@ -0,0 +1,20 @@
+package edu.usc.nlcaceres.infectionprevention.composables.views
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import edu.usc.nlcaceres.infectionprevention.composables.listItems.OnRemoveSelectedFilter
+import edu.usc.nlcaceres.infectionprevention.composables.listItems.SelectedFilterList
+import edu.usc.nlcaceres.infectionprevention.viewModels.ViewModelReportList
+
+/** Used in FragmentReportList to visualize sorters and filters chosen in FragmentSortFilter
+ * Once set, they are used to sort and filter the Reports
+ * Chosen sorters and filters can be removed on tap
+**/
+@Composable
+fun SorterFilterListView(modifier: Modifier = Modifier, viewModel: ViewModelReportList = viewModel(), onClick: OnRemoveSelectedFilter) {
+  SelectedFilterList(viewModel.selectedFilters, modifier) { position, filter ->
+    viewModel.selectedFilters.removeAt(position) // First remove filter from selectedFilter List
+    onClick(position, filter) // THEN let Parent View of the ComposeView containing this SorterFilterListView run its own callback
+  }
+}
