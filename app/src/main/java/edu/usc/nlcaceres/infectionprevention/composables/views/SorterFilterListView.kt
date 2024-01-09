fa5310f166bad444a3f9f84521f9fb0499275562
NLCaceres <NLCaceres21@Gmail.com>
2024-01-08 23:50:06 -0800

    Add TestTag to Screen-level SorterFilterListView for UI-Testing helper ReportListScreen to use

--- app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/views/SorterFilterListView.kt
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/views/SorterFilterListView.kt
@@ -2,6 +2,7 @@ package edu.usc.nlcaceres.infectionprevention.composables.views
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.lifecycle.viewmodel.compose.viewModel
 import edu.usc.nlcaceres.infectionprevention.composables.listItems.OnRemoveSelectedFilter
 import edu.usc.nlcaceres.infectionprevention.composables.listItems.SelectedFilterList
@@ -13,7 +14,7 @@ import edu.usc.nlcaceres.infectionprevention.viewModels.ViewModelReportList
 **/
 @Composable
 fun SorterFilterListView(modifier: Modifier = Modifier, viewModel: ViewModelReportList = viewModel(), onClick: OnRemoveSelectedFilter) {
-  SelectedFilterList(viewModel.selectedFilters, modifier) { position, filter ->
+  SelectedFilterList(viewModel.selectedFilters, modifier.then(Modifier.testTag("SorterFilterListView"))) { position, filter ->
     viewModel.selectedFilters.removeAt(position) // First remove filter from selectedFilter List
     onClick(position, filter) // THEN let Parent View of the ComposeView containing this SorterFilterListView run its own callback
   }
