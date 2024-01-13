55b7014f9d90c35a11295742d68dd6a197fcb7c0
NLCaceres <NLCaceres21@Gmail.com>
2024-01-12 23:34:34 -0800

    Add TestTag to screen-level SelectedFilterListView for UI-Test helper SortFilterScreen to use

--- app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/views/SelectedFilterListView.kt
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/views/SelectedFilterListView.kt
@@ -2,6 +2,7 @@ package edu.usc.nlcaceres.infectionprevention.composables.views
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.lifecycle.viewmodel.compose.viewModel
 import edu.usc.nlcaceres.infectionprevention.composables.listItems.OnRemoveSelectedFilter
 import edu.usc.nlcaceres.infectionprevention.composables.listItems.SelectedFilterList
@@ -14,7 +15,7 @@ import edu.usc.nlcaceres.infectionprevention.viewModels.ViewModelSortFilter
 //? the Stable `selectedFilterList` into SelectedFilterList, it DOESN'T seem to trigger SelectedFilterListFragment to also recompose
 @Composable
 fun SelectedFilterListView(modifier: Modifier = Modifier, viewModel: ViewModelSortFilter = viewModel(), onClick: OnRemoveSelectedFilter) {
-  SelectedFilterList(viewModel.selectedFilterList, modifier) { position, filter ->
+  SelectedFilterList(viewModel.selectedFilterList, modifier.then(Modifier.testTag("SelectedFilterListView"))) { position, filter ->
     viewModel.removeSelectedFilter(position) // First remove filter from selectedFilterList
     onClick(position, filter) // THEN let Parent View of the ComposeView containing this SelectedFilterListFrag run its own callback
   }
