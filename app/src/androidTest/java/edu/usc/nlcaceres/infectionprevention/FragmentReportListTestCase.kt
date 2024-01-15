c52c08e8fd71175c5d8a119d8f84d50ef1eadf19
NLCaceres <NLCaceres21@Gmail.com>
2024-01-14 21:53:06 -0800

    Update so all funcs use Kaspresso's run() + step() for better readability/organization

--- app/src/androidTest/java/edu/usc/nlcaceres/infectionprevention/FragmentReportListTestCase.kt
+++ app/src/androidTest/java/edu/usc/nlcaceres/infectionprevention/FragmentReportListTestCase.kt
@@ -12,6 +12,7 @@ import edu.usc.nlcaceres.infectionprevention.screens.MainActivityScreen
 import edu.usc.nlcaceres.infectionprevention.screens.NavDrawerScenario
 import edu.usc.nlcaceres.infectionprevention.screens.ReportListScreen
 import edu.usc.nlcaceres.infectionprevention.screens.SettingsScreen
+import edu.usc.nlcaceres.infectionprevention.screens.SortFilterScreen
 import edu.usc.nlcaceres.infectionprevention.util.RepositoryModule
 import io.github.kakaocup.kakao.edit.KEditText
 import org.junit.Assert.assertEquals
@@ -27,9 +28,12 @@ class FragmentReportListTestCase: TestCase(kaspressoBuilder = Kaspresso.Builder.
   @get:Rule(order = 1)
   val composeTestRule = createAndroidComposeRule<ActivityMain>()
 
-  @Before fun navigateToReportList() {
-    run { scenario(NavDrawerScenario { goToReportList() }) }
-    ReportListScreen { sorryMessageTV.isInvisible(); assertEquals(5, reportList.getSize()) }
+  @Before fun navigateToReportList() = run {
+    step("Navigate to ReportList") { scenario(NavDrawerScenario { goToReportList() }) }
+    step("Check ReportList loaded") {
+      device.uiDevice.waitForIdle() // Wait for NavDrawer to close and ReportList to animate in
+      ReportListScreen { sorryMessageTV.isInvisible(); assertEquals(5, reportList.getSize()) }
+    }
   }
 
   //! Navigation
@@ -37,114 +41,129 @@ class FragmentReportListTestCase: TestCase(kaspressoBuilder = Kaspresso.Builder.
     ReportListScreen { sortFilterButton.click() }
     //TODO: FragmentSortFilter checks
   }
-  @Test fun navigate_To_Settings() {
-    ReportListScreen { settingsButton.click() }
-    SettingsScreen {
-      personalInfoTitle { hasText("Personal Info") }
-      adminInfoTitle { isVisible() }
+  @Test fun navigate_To_Settings() = run {
+    step("Click Actionbar SettingsButton to navigate to SettingsScreen") {
+      ReportListScreen.settingsButton.click()
+    }
+    step("Check SettingsScreen loaded") {
+      SettingsScreen {
+        rvContainer.isDisplayed()
+        personalInfoTitle { hasText("Personal Info") }
+        adminInfoTitle { isVisible() }
+      }
     }
   }
-  @Test fun navigate_Back_To_Home_Page() {
-    ReportListScreen { pressBack() }
-    MainActivityScreen {
-      assertEquals(2, precautionRV.getSize())
-      precautionRV.lastChild<MainActivityScreen.PrecautionRvItem> {
-        assertEquals(4, healthPracticeRV.getSize())
+  @Test fun navigate_Back_To_Home_Page() = run {
+    step("Press back button from ReportListScreen") {
+      ReportListScreen.pressBack()
+    }
+    step("Check MainActivityScreen is visible") {
+      MainActivityScreen {
+        assertEquals(2, precautionRV.getSize())
+        precautionRV.lastChild<MainActivityScreen.PrecautionRvItem> {
+          assertEquals(4, healthPracticeRV.getSize())
+        }
       }
     }
   }
 
   //! Searchbar Filtering
-  @Test fun expand_SearchBar_ActionView_When_Empty() {
-    ReportListScreen {
-      searchButton.click() // Expands searchBar
-      searchBar.isDisplayed()
-      searchBar.hasText("")
-      searchBar.isFocused()
-      searchBarCloseButton.click() // Closes the searchBar via ActionBar collapse
-      device.uiDevice.waitForIdle()
-      searchBar.doesNotExist()
-
-      searchButton.click() // Expands searchBar
-      searchBar.isDisplayed()
-      searchBar.hasText("")
-      searchBar.isFocused()
-      reportList.click() // WHEN tapping away from the searchBar
-      searchBar.isDisplayed() // THEN it DOESN'T hide the searchBar
-      searchBar.isFocused() // AND focus is retained!
+  @Test fun expand_SearchBar_ActionView_When_Empty() = run {
+    step("Click the search button to expand and focus the empty search bar") {
+      ReportListScreen {
+        searchButton.click()
+        searchBar.isDisplayed()
+        searchBar.hasText("")
+        searchBar.isFocused()
+      }
     }
-  }
-  @Test fun expand_SearchBar_ActionView_With_Text() {
-    run {
-      step("Expand searchBar and check if displayed, focused, and empty") {
-        ReportListScreen {
-          searchButton.click()
-          searchBar.isDisplayed()
-          searchBar.hasText("")
-          searchBar.isFocused()
-        }
+    step("Close the search bar by clicking its close button") {
+      ReportListScreen {
+        searchBarCloseButton.click()
+        searchBar.doesNotExist()
       }
-      step("Enter 'USC' search term and get 2 reports") {
-        ReportListScreen {
-          searchBar.typeText("USC")
-          device.uiDevice.waitForIdle() //? Useful when UI is running animations, especially ones you can't control like typing
-          assertEquals(2, reportList.getSize())
-        }
+    }
+    step("Click the search button to expand and focus the empty search bar") {
+      ReportListScreen {
+        searchButton.click()
+        searchBar.isDisplayed()
+        searchBar.hasText("")
+        searchBar.isFocused()
       }
-      step("Can tap away from searchBar BUT it remains open until its close button tapped") {
-        ReportListScreen {
-          reportList.click()
-          searchBar.isDisplayed()
-          searchBarCloseButton.click()
-          device.uiDevice.waitForIdle()
-          searchBar.doesNotExist()
-        }
-      }
-      step("Reopen searchBar after filling BUT find an empty searchbar") {
-        ReportListScreen {
-          searchButton.click()
-          searchBar.isDisplayed()
-          searchBar.hasText("")
-        }
+    }
+    step("Click away from the search bar and check it stays open and focused") {
+      ReportListScreen {
+        reportList.click()
+        searchBar.isDisplayed()
+        searchBar.isFocused()
       }
     }
   }
-  @Test fun filter_With_SearchBar() {
-    run {
-      step("Expand searchBar, check if empty and displayed, and check report list has 5 reports") {
-        ReportListScreen {
-          searchButton.click()
-          searchBar.isDisplayed()
-          searchBar.hasText("")
-          assertEquals(5, reportList.getSize())
-        }
+  @Test fun expand_SearchBar_ActionView_With_Text() = run {
+    step("Expand searchBar and check if displayed, focused, and empty") {
+      ReportListScreen {
+        searchButton.click()
+        searchBar.isDisplayed()
+        searchBar.hasText("")
+        searchBar.isFocused()
       }
-      step("Enter 'HSC' searchTerm and get 3 reports") {
-        ReportListScreen {
-          searchBar.typeText("HSC")
-          device.uiDevice.waitForIdle()
-          assertEquals(3, reportList.getSize())
-        }
+    }
+    step("Enter 'USC' search term and get 2 reports") {
+      ReportListScreen {
+        searchBar.typeText("USC")
+        device.uiDevice.waitForIdle() //? Useful when UI is running animations, especially ones you can't control like typing
+        assertEquals(2, reportList.getSize())
       }
-      step("Close the searchBar and get 5 reports again") {
-        ReportListScreen {
-          searchBarCloseButton.click()
-          device.uiDevice.waitForIdle()
-          searchBar.doesNotExist()
-          assertEquals(5, reportList.getSize())
-        }
+    }
+    step("Can tap away from searchBar BUT it remains open until its close button tapped") {
+      ReportListScreen {
+        reportList.click()
+        searchBar.isDisplayed()
+        searchBarCloseButton.click()
+        searchBar.doesNotExist()
       }
-      step("Reopen the searchBar, checking if its visible, empty, and especially focused so typing 'USC' just works") {
-        ReportListScreen {
-          searchButton.click()
-          searchBar.isDisplayed()
-          searchBar.hasText("") // WHEN re-opened, searchBar expected to be autofocused
-          val expectedSearchBar = KEditText { withMatcher(ViewMatchers.isFocused()) }
-          expectedSearchBar.typeText("USC") // THEN autofocused view will be searchBar allowing typing
-          device.uiDevice.waitForIdle()
-          assertEquals(2, reportList.getSize())
-          searchBarCloseButton.click()
-        }
+    }
+    step("Reopen searchBar after filling BUT find an empty searchbar") {
+      ReportListScreen {
+        searchButton.click()
+        searchBar.isDisplayed()
+        searchBar.hasText("")
+      }
+    }
+  }
+  @Test fun filter_With_SearchBar() = run {
+    step("Expand searchBar, check if empty and displayed, and check report list has 5 reports") {
+      ReportListScreen {
+        searchButton.click()
+        searchBar.isDisplayed()
+        searchBar.hasText("")
+        assertEquals(5, reportList.getSize())
+      }
+    }
+    step("Enter 'HSC' searchTerm and get 3 reports") {
+      ReportListScreen {
+        searchBar.typeText("HSC")
+        device.uiDevice.waitForIdle()
+        assertEquals(3, reportList.getSize())
+      }
+    }
+    step("Close the searchBar and get 5 reports again") {
+      ReportListScreen {
+        searchBarCloseButton.click()
+        searchBar.doesNotExist()
+        assertEquals(5, reportList.getSize())
+      }
+    }
+    step("Reopen the searchBar, checking if its visible, empty, and especially focused so typing 'USC' just works") {
+      ReportListScreen {
+        searchButton.click()
+        searchBar.isDisplayed()
+        searchBar.hasText("") // WHEN re-opened, searchBar expected to be autofocused
+        val expectedSearchBar = KEditText { withMatcher(ViewMatchers.isFocused()) }
+        expectedSearchBar.typeText("USC") // THEN autofocused view will be searchBar allowing typing
+        device.uiDevice.waitForIdle()
+        assertEquals(2, reportList.getSize())
+        searchBarCloseButton.click()
       }
     }
   }
