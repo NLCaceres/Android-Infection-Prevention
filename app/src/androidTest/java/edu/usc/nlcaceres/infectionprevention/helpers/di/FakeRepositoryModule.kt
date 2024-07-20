0d04682d82c3be3a0238e7c2dbcce8641093e262
NLCaceres <NLCaceres21@Gmail.com>
2024-07-20 12:05:04 -0700

    Update Hilt RepositoryModule import + explain stubbing issue

--- app/src/androidTest/java/edu/usc/nlcaceres/infectionprevention/helpers/di/FakeRepositoryModule.kt
+++ app/src/androidTest/java/edu/usc/nlcaceres/infectionprevention/helpers/di/FakeRepositoryModule.kt
@@ -4,14 +4,19 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
-import javax.inject.Singleton
 import edu.usc.nlcaceres.infectionprevention.data.EmployeeRepository
 import edu.usc.nlcaceres.infectionprevention.data.HealthPracticeRepository
 import edu.usc.nlcaceres.infectionprevention.data.LocationRepository
 import edu.usc.nlcaceres.infectionprevention.data.PrecautionRepository
 import edu.usc.nlcaceres.infectionprevention.data.ReportRepository
-import edu.usc.nlcaceres.infectionprevention.util.RepositoryModule
+import edu.usc.nlcaceres.infectionprevention.hilt.RepositoryModule
+import javax.inject.Singleton
 
+/** Stubs in data across the InstrumentedTests using Hilt's preferred method of @TestInstallIn */
+
+// - Important to use the SingletonComponent, not the original ViewModelComponent
+// Since ViewModelComponent seems to cause ActivityMainRenderProgressTestCase to fail due to Field @Inject
+// It MAY be fixable via nested @Install Modules in each TestCase BUT that bloats the other TestCases just to fix 1 TestCase
 @Module
 @TestInstallIn(components = [SingletonComponent::class], replaces = [RepositoryModule::class])
 object FakeRepositoryModule {
