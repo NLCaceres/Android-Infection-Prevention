6d97e74a5e506aa420f60ce3bbeeb89e6cdfbd9f
NLCaceres <NLCaceres21@Gmail.com>
2024-09-22 01:11:15 -0700

    Swap base OutlinedTextField with the reusable app version

--- app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/common/MaterialSpinner.kt
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/common/MaterialSpinner.kt
@@ -1,10 +1,8 @@
 package edu.usc.nlcaceres.infectionprevention.composables.common
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -12,9 +10,7 @@ import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MenuAnchorType
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,10 +18,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.offset
 import edu.usc.nlcaceres.infectionprevention.data.HealthPractice
 import edu.usc.nlcaceres.infectionprevention.data.Precaution
 import edu.usc.nlcaceres.infectionprevention.ui.theme.AppTheme
@@ -41,23 +35,10 @@ fun <T> MaterialSpinner(title: String, options: List<T>, onSelect: (option: T) -
     expanded = expanded, onExpandedChange = { expanded = it },
     modifier = Modifier.then(modifier)
   ) {
-    OutlinedTextField( // - menuAnchor.PrimaryNotEditable helps a Read-Only TextField expand/collapse the menu
-      modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable),
-      value = selectedOption.toString(), onValueChange = {}, readOnly = true, singleLine = true,
-      label = { Text(title, Modifier
-          .layout { measurable, constraints ->
-            val xOffset = (-4).dp.roundToPx()
-            val placeable = measurable.measure(constraints.offset(-xOffset * 2))
-            layout(placeable.width + xOffset * 2, placeable.height) {
-              placeable.place(xOffset, 1.3.dp.roundToPx())
-            }
-          }
-          .background(MaterialTheme.colorScheme.secondaryContainer, RoundedCornerShape(2.dp, 2.dp))
-          .padding(2.dp, 0.5.dp, 2.dp),
-          style = MaterialTheme.typography.labelSmall)
-      },
-      trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded, Modifier.scale(1.5f)) },
-      colors = appOutlinedTextFieldColors()
+    AppOutlinedTextField(
+      selectedOption.toString(), title,
+      Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable), // - Helps a Read-Only TextField expand/collapse the menu
+      trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded, Modifier.scale(1.5f)) }
     )
     ExposedDropdownMenu(
       expanded = expanded, onDismissRequest = { expanded = false },
@@ -82,21 +63,6 @@ fun <T> MaterialSpinner(title: String, options: List<T>, onSelect: (option: T) -
     }
   }
 }
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-fun appOutlinedTextFieldColors(): TextFieldColors {
-  return ExposedDropdownMenuDefaults.outlinedTextFieldColors(
-    focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-    unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-    unfocusedBorderColor = MaterialTheme.colorScheme.onSecondaryContainer,
-    focusedLabelColor = MaterialTheme.colorScheme.tertiary,
-    focusedBorderColor = MaterialTheme.colorScheme.tertiary,
-    focusedTrailingIconColor = MaterialTheme.colorScheme.tertiary,
-    unfocusedTrailingIconColor = MaterialTheme.colorScheme.onSecondaryContainer
-  )
-}
-
 
 @Preview(widthDp = 320, heightDp = 500, showBackground = true)
 @Composable
