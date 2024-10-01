11e2fd1569d4a06160a4d73972ee24765dc04e34
NLCaceres <NLCaceres21@Gmail.com>
2024-10-01 01:23:04 -0700

    Add doc comment + `modifier` param & padding based on expected usage
    -Documentation comment explains expected usage and placement
    -Modifier param conforms to Jetpack Compose guidelines as 1st optional
    parameter BUT a padding Modifier is added based on how this Button
    SHOULD be used in a Dialog composable. If the padding needs to be
    overridden, then an offset Modifier should work

--- app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/common/buttons/NegativeButton.kt
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/common/buttons/NegativeButton.kt
@@ -1,16 +1,26 @@
 package edu.usc.nlcaceres.infectionprevention.composables.common.buttons
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import edu.usc.nlcaceres.infectionprevention.ui.theme.AppTheme
 
+/**
+ A Negative Button used by Dialog Composables containing "Cancel" as text
+ typically placed left of a Positive "OK" or "Confirmation" button
+ */
 @Composable
-fun NegativeButton(onClick: () -> Unit) {
-  AppFilledTonalButton(onClick, "Cancel", colors = negativeButtonColors())
+fun NegativeButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+  AppFilledTonalButton( // - Setting padding here due to expected placement BUT can be offset via `modifier`
+    onClick, "Cancel", Modifier.padding(horizontal = 10.dp, vertical = 0.dp).then(modifier),
+    negativeButtonColors()
+  )
 }
 
 @Composable
@@ -26,7 +36,7 @@ fun negativeButtonColors(): ButtonColors {
 private fun NegativeButtonPreview() {
   AppTheme {
     Column {
-      NegativeButton {}
+      NegativeButton({})
     }
   }
 }
