869de1290e7def6a030aae6e4f9324c4522678de
NLCaceres <NLCaceres21@Gmail.com>
2024-10-06 23:55:38 -0700

    Create a reusable DatePickerDialog

---
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/common/dialogs/AppDatePickerDialog.kt
@@ -0,0 +1,52 @@
+package edu.usc.nlcaceres.infectionprevention.composables.common.dialogs
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.usc.nlcaceres.infectionprevention.composables.common.AppOutlinedTextField
+import edu.usc.nlcaceres.infectionprevention.composables.common.buttons.NegativeButton
+import edu.usc.nlcaceres.infectionprevention.composables.common.buttons.PositiveButton
+import edu.usc.nlcaceres.infectionprevention.ui.theme.AppTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppDatePickerDialog(
+  state: DatePickerState, onDateSelected: (Long?) -> Unit, onDismiss: () -> Unit,
+  modifier: Modifier = Modifier
+) {
+  DatePickerDialog(
+    onDismiss,
+    { PositiveButton({ onDateSelected(state.selectedDateMillis); onDismiss() }) },
+    Modifier.then(modifier),
+    { NegativeButton(onDismiss) }
+  ) {
+    DatePicker(state)
+  }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(widthDp = 400, heightDp = 600, showBackground = true)
+@Composable
+private fun AppDatePickerDialogPreview() {
+  var isVisible by remember { mutableStateOf(false) }
+  val datePickerState = rememberDatePickerState()
+
+  AppTheme {
+    Column {
+      AppOutlinedTextField("", "Foo", readOnly = true, onClick = { isVisible = true })
+      if (isVisible) {
+        AppDatePickerDialog(datePickerState, { isVisible = false }, { isVisible = false })
+      }
+    }
+  }
+}
