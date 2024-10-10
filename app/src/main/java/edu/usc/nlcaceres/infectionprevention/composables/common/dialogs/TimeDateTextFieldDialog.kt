566c8bb3c095f7ebdc634274e64925eb28fafaa8
NLCaceres <NLCaceres21@Gmail.com>
2024-10-09 23:41:36 -0700

    Setup Composable combining TextField with Time & Date Picker Dialogs
    -Sets up the layout and general UX as the TextField would be used in a
    form to specify the time and date
    -Begin switching to modern `java.time` library via LocalDateTime

---
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/common/dialogs/TimeDateTextFieldDialog.kt
@@ -0,0 +1,40 @@
+package edu.usc.nlcaceres.infectionprevention.composables.common.dialogs
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+import edu.usc.nlcaceres.infectionprevention.composables.common.AppOutlinedTextField
+import edu.usc.nlcaceres.infectionprevention.ui.theme.AppTheme
+import java.time.LocalDateTime
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimeDateTextFieldDialog() {
+  var isVisible by remember { mutableStateOf(false) }
+  val localDateTime = LocalDateTime.now()
+  val timePickerState = rememberTimePickerState(
+    localDateTime.hour, localDateTime.minute, is24Hour = false
+  )
+  val datePickerState = rememberDatePickerState()
+  Column {
+    AppOutlinedTextField("", "Select a Time & Date", onClick = { isVisible = true })
+    if (isVisible) {
+      TimeDatePickerDialog(timePickerState, datePickerState, {}, {}, { isVisible = false })
+    }
+  }
+}
+
+@Preview(widthDp = 400, heightDp = 600, showBackground = true)
+@Composable
+private fun TimeDateTextFieldDialogPreview() {
+  AppTheme {
+    TimeDateTextFieldDialog()
+  }
+}
