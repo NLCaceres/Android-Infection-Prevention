ff94af789d02f832547f30c259a1cb67354627b9
NLCaceres <NLCaceres21@Gmail.com>
2024-10-12 18:14:46 -0700

    Add a Time and Date formatter based on TimePicker and DatePicker state
    -Both formatters use more reusable funcs under the hood to do the actual
    conversion and formatting into readable Strings. These reusable
    formatters are available for other composables to use

--- app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/util/DateTime.kt
+++ app/src/main/java/edu/usc/nlcaceres/infectionprevention/composables/util/DateTime.kt
@@ -1,9 +1,15 @@
 package edu.usc.nlcaceres.infectionprevention.composables.util
 
+import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SelectableDates
+import androidx.compose.material3.TimePickerState
 import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalTime
 import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 val DatesUntilNow: SelectableDates = object: SelectableDates {
@@ -16,4 +22,25 @@ val DatesUntilNow: SelectableDates = object: SelectableDates {
     val instant = Instant.now()
     return instant.atZone(ZoneId.systemDefault()).year >= year
   }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+fun formattedTime(time: TimePickerState): String {
+  return formattedTime(time.hour, time.minute)
+}
+fun formattedTime(hour: Int, minute: Int): String {
+  val localTime = LocalTime.of(hour, minute)
+  val formatter = DateTimeFormatter.ofPattern("h:mm a").withZone(ZoneId.systemDefault())
+  return localTime.format(formatter)
+}
+@OptIn(ExperimentalMaterial3Api::class)
+fun formattedDate(date: DatePickerState): String {
+  return formattedDate(date.selectedDateMillis)
+}
+fun formattedDate(dateUtcMilli: Long?): String {
+  if (dateUtcMilli == null) { return "" }
+  val instant = Instant.ofEpochMilli(dateUtcMilli)
+  val localDate = LocalDate.ofInstant(instant, ZoneOffset.UTC)
+  val formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy")
+  return localDate.format(formatter)
 }
